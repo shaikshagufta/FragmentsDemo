cc8e8fe11b3f293b358219043ba8dd2fbe45b80f
shaikshagufta <shaikshagufta99@gmailcom>
2022-06-02 00:29:24 +0530

    Programmatically adding Fragment to activity

--- app/src/main/java/com/example/fragmentsdemo/MainActivity.kt
+++ app/src/main/java/com/example/fragmentsdemo/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.fragmentsdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addFragment()
+    }
+    private fun addFragment() {
+        val fragmentManager : FragmentManager = supportFragmentManager
+        val fragmentTransaction :FragmentTransaction = fragmentManager.beginTransaction()
+        val sampleFragment: Fragment = SampleFragment()
+        fragmentTransaction.add(R.id.fragment_container, sampleFragment)
     }
 }
